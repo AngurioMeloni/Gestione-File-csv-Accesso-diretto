362ce9e023b30b0c409f179179be6f5562648ba7
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-09-12 21:23:09 +0200

    Istruzione 4 5

--- Class1.cs
+++ Class1.cs
@@ -113,5 +113,53 @@ namespace Crud_File_csv_Accesso_diretto
             reader.Close();
             return lm;
         }
+        public static void Istruzione4()
+        {
+            string s;
+            int i = 0;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            StreamWriter writer = new StreamWriter("temporaneo.csv");
+
+            s = reader.ReadLine();
+            while (s != null)
+            {
+                if (i != 0)
+                {
+                    writer.WriteLine(s.PadRight(200));
+                }
+                else
+                {
+                    writer.WriteLine(s.PadRight(200));
+                }
+
+                s = reader.ReadLine();
+                i++;
+            }
+
+            reader.Close();
+            writer.Close();
+
+            File.Replace("temporaneo.csv", "masserini1.csv", "backup.csv");
+        }
+        public static void Istruzione5(string anno, string nazioni, string MKwh, string note, string Vrandom, string Vbooleano)
+        {
+            int i = 0;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                i++;
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            var oStream = new FileStream("masserini1.csv", FileMode.Append, FileAccess.Write, FileShare.Read);
+            BinaryWriter writer = new BinaryWriter(oStream);
+            string l = $"{anno};{nazioni};{MKwh};{note};{Vrandom};{Vbooleano};{i}".PadRight(200);
+            byte[] data = Encoding.ASCII.GetBytes(l);
+            writer.Write(data);
+
+            writer.Close();
+            oStream.Close();
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,5 +47,20 @@ namespace Crud_File_csv_Accesso_diretto
                 listView1.Items.Add(MaxC[i].ToString());//visualizzazione dei valori dell'array nella listview
             }
         }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Class1.Istruzione4();//richiamo alla funzione 4
+            MessageBox.Show("Istruzione 4 eseguita con successo");//messaggio di output che indica che la funzione 4 è stata eseguita con successo
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            groupBox1.Show();//funzione che mostra la groupbox1
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Class1.Istruzione5(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);//richiamo alla funzione 5
+            MessageBox.Show("Istruzione 5 eseguita con successo");//messaggio di output che indica che la funzione 5 è stata eseguita con successo
+        }
+
     }
 }
