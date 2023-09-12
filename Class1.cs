cc1daf9cda42a8d81edb4941e27dbd7b606674f0
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-09-12 21:20:34 +0200

    Istruzioni 2 3 3A

--- Class1.cs
+++ Class1.cs
@@ -43,5 +43,75 @@ namespace Crud_File_csv_Accesso_diretto
 
             return c;
         }
+        public static int Istruzione2()
+        {
+            string s;
+            int num;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            s = reader.ReadLine();
+            reader.Close();
+            return num = s.Split(';').Length;
+        }
+        public static int Istruzione3()
+        {
+            StreamReader reader = new StreamReader("masserini1.csv");
+            int ls = 0, lm = 0, i = 0;
+            string s;
+
+            s = reader.ReadLine();
+            while (s != null)
+            {
+                ls = s.Length;
+                if (i != 0)
+                {
+                    if (lm < ls)
+                    {
+                        lm = s.Length;
+                    }
+                }
+                s = reader.ReadLine();
+                i++;
+            }
+            reader.Close();
+            return lm;
+        }
+        public static int[] Istruzione3A()
+        {
+            StreamReader reader = new StreamReader("masserini1.csv");
+            string r = reader.ReadLine();
+            int[] lm = new int[Istruzione2()];
+            int a = 0;
+            r = reader.ReadLine();
+
+            while (r != null)
+            {
+                string[] split = r.Split(';');
+                string[] array = new string[Istruzione2()];
+
+                for (int i = 0; i < Istruzione2(); i++)
+                {
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                    r = reader.ReadLine();
+                    a = 0;
+                    while (r != null)
+                    {
+                        string[] stringaSplit = r.Split(';');
+                        if (a != 0)
+                        {
+                            if (lm[i] < stringaSplit[i].Length)
+                            {
+                                lm[i] = stringaSplit[i].Length;
+                            }
+
+                        }
+                        r = reader.ReadLine();
+                        a++;
+                    }
+                }
+            }
+            reader.Close();
+            return lm;
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,25 @@ namespace Crud_File_csv_Accesso_diretto
             Class1.Istruzione1();
             MessageBox.Show("Istruzione eseguita correttamente");
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Il numero di campi è: " + Class1.Istruzione2());
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Il numero di caratteri è: " + Class1.Istruzione3());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int contatore = 7;
+            listView1.Clear(); // pulizia della listview
+            int[] MaxC = new int[contatore];//Dichiaro l'array massimoo numero di caratteri(MaxC)
+            MaxC = Class1.Istruzione3A();//pongo l'array uguale alla funzione Istruzione3A
+            for (int i = 0; i < contatore; i++)//ciclo for che mi permette di visualizzare i valori dell'array nella listview
+            {
+                listView1.Items.Add(MaxC[i].ToString());//visualizzazione dei valori dell'array nella listview
+            }
+        }
     }
 }
